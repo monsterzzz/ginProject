185416ed9be115e2d3bfca481d3f7b6a9984c8d5
monsterzzz <monsterxmonster@163.com>
2019-08-31 01:04:41 +0800

    ...

--- models/tag.go
+++ models/tag.go
@@ -1,40 +1,67 @@
 package models
 
+import (
+	"github.com/jinzhu/gorm"
+	"time"
+)
+
 type Tag struct {
 	Model
-	Name string  `json:"name"`
-	CreatedBy string `json:"created_by"`
+	Name       string `json:"name"`
+	CreatedBy  string `json:"created_by"`
 	ModifiedBy string `json:"modified_by"`
-	State int `json:"state"`
+	State      int    `json:"state"`
 }
 
-
-func GetTags(pageNum int, pageSize int, maps interface {}) (tags []Tag) {
+func GetTags(pageNum int, pageSize int, maps interface{}) (tags []Tag) {
 	db.Where(maps).Offset(pageNum).Limit(pageSize).Find(&tags)
 	return
 }
-func GetTagTotal(maps interface {}) (count int){
+func GetTagTotal(maps interface{}) (count int) {
 	db.Model(&Tag{}).Where(maps).Count(&count)
 	return
 }
 
-func TagExists(name string) (flag bool){
+func TagExistsByName(name string) (flag bool) {
 	var tmp Tag
-	db.Select("id").Where("name = ?",name).First(&tmp)
+	db.Select("id").Where("name = ?", name).First(&tmp)
 	if tmp.ID > 0 {
 		flag = true
 	}
 	return flag
 }
 
-func AddTag(name string,state int,createBy string){
+func TagExistsById(id int) (flag bool) {
+	var tmp Tag
+	db.Select("id").Where("id = ?", id).First(&tmp)
+	if tmp.ID > 0 {
+		flag = true
+	}
+	return flag
+}
+
+func AddTag(name string, state int, createBy string) {
 	db.Create(&Tag{
-		Name:name,
-		State:state,
-		CreatedBy:createBy,
+		Name:      name,
+		State:     state,
+		CreatedBy: createBy,
 	})
 }
 
-func editTag(tag *Tag,id int)  {
-	db.Model(&tag).Where("id = ?",id).Update(&tag)
+func EditTag(tag *Tag, id int) {
+	db.Model(&tag).Where("id = ?", id).Update(&tag)
+}
+
+func DeleteTag(id int) {
+	db.Where("id = ?", id).Delete(&Tag{})
+}
+
+func (tag *Tag) BeforeCreate(scpoe *gorm.Scope) error {
+	scpoe.SetColumn("CreatedOn", time.Now().Unix())
+	return nil
+}
+
+func (tag *Tag) BeforeUpdate(scope *gorm.Scope) error {
+	scope.SetColumn("ModifiedOn", time.Now().Unix())
+	return nil
 }
--- routers/api/v1/tags.go
+++ routers/api/v1/tags.go
@@ -55,15 +55,15 @@ func AddTag(c *gin.Context)  {
 	code := e.INVALID_PARAMS
 
 	if !valid.HasErrors(){
-		if !models.TagExists(name){
+		if !models.TagExistsByName(name){
 			code = e.SUCCESS
 			models.AddTag(name,state,createdBy)
 		}else {
 			code = e.ERROR_EXIST_TAG
 		}
 	}else {
-		for _,e := range valid.Errors{
-			log.Info(e.Key,e.Message)
+		for _,err := range valid.Errors{
+			log.Info(err.Key,err.Message)
 		}
 	}
 
@@ -76,9 +76,61 @@ func AddTag(c *gin.Context)  {
 }
 
 func EditTag(c *gin.Context)  {
-	
+	name := c.Query("name")
+	state := com.StrTo(c.DefaultQuery("state","0")).MustInt()
+	ModifiedBy := c.Query("modified_by")
+	id := com.StrTo(c.Param("id")).MustInt()
+
+	valid := validation.Validation{}
+	valid.Required(name,"name").Message("can not be nil")
+	valid.Required(id,"id").Message("id can not be nil")
+	valid.MaxSize(name,100,"name").Message("can not more than 100")
+	valid.Required(ModifiedBy, "created_by").Message("修改人不能为空")
+	valid.MaxSize(ModifiedBy, 100, "created_by").Message("修改人最长为100字符")
+	valid.Range(state, 0, 1, "state").Message("状态只允许0或1")
+
+	code := -1
+	if !valid.HasErrors(){
+		if !models.TagExistsById(id){
+			code = e.SUCCESS
+			models.EditTag(&models.Tag{
+				Name:name,
+				State:state,
+				ModifiedBy:ModifiedBy,
+			},id)
+		}else {
+			code = e.ERROR_EXIST_TAG
+		}
+	}else {
+		for _,err := range valid.Errors{
+			log.Info(err.Key,err.Message)
+		}
+	}
+
+	c.JSON(http.StatusOK,jsonRespMaker(code,make(map[string]string)))
 }
 
 func DeleteTag(c *gin.Context){
+	id := com.StrTo(c.Param("id")).MustInt()
 
+	valid := validation.Validation{}
+
+	valid.Required(id,"id").Message("id can not be nil")
+	code := -1
+	if !models.TagExistsById(id){
+		code = e.ERROR_NOT_EXIST_TAG
+	}else {
+		code = e.SUCCESS
+		models.DeleteTag(id)
+	}
+
+	c.JSON(http.StatusOK,jsonRespMaker(code,make(map[string]string)))
+}
+
+func jsonRespMaker(code int,data interface{})(h gin.H){
+	return gin.H{
+		"code" : code,
+		"msg" : e.GetMsg(code),
+		"data" : data,
+	}
 }
